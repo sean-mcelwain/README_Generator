5eacfef36e8d57d859ddca2e2d086857984ad67d
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-05-24 13:05:51 -0500

    update

--- index.js
+++ index.js
@@ -6,9 +6,41 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 
 const text = "text";
-const questions = "blue" + text;
+const questionInquirer = "blue" + text;
 
-const template = questions + text;
+
+const title = "# Project Title  \n";
+const description = "# Description \n Description Text  \n";
+const tableOfContents = 
+"# Table of Contents  \n"
++" 1. [ Description. ](#description)  \n" 
++" 2. [ Installation. ](#description)  \n" 
++" 3. [ Usage tips. ](#usage)  \n"
++" 4. [ License. ](#license)  \n"
++" 5. [ Contribution Guidelines. ](#contribution)  \n"
++" 6. [ Tests. ](#tests)  \n"
++" 7. [ Questions. ](#questions)  \n"
++" 8. [ Deployment Location. ](#link)  \n" 
++" 9. [ Screenshot. ](#screenshot)  \n"
+
+const installation = "<a name='installation'></a> # Installation \n Installation Text  \n"
+const usage = "<a name='usage'></a> # Usage \n Usage Text  \n"
+const license = "<a name='license'></a> # License \n License Text  \n"
+const contribution = "<a name='contribution'></a> # Contribution Guidelines \n Guidelines Text  \n"
+const tests = "<a name='tests'></a> # Tests \n Test Text  \n"
+const questions = "<a name='tests'></a> # Tests \n Test Text  \n"
+const link = "<a name='link'></a> # Deployment Location \n URL  \n"
+const screenshot = "<a name='screenshot'></a> # Screenshot: \n ![Screenshot](https://github.com/sean-mcelwain/weather_dashboard/blob/main/assets/images/screenshot.jpg)  \n"
+
+
+
+
+
+
+
+
+const template = title + description + tableOfContents + installation + usage + license + contribution + tests
+                + questions + link + screenshot;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- newfile.md
+++ newfile.md
@@ -1,1 +1,29 @@
-bluetexttext
+# Project Title  
+# Description 
+ Description Text  
+# Table of Contents  
+ 1. [ Description. ](#description)  
+ 2. [ Installation. ](#description)  
+ 3. [ Usage tips. ](#usage)  
+ 4. [ License. ](#license)  
+ 5. [ Contribution Guidelines. ](#contribution)  
+ 6. [ Tests. ](#tests)  
+ 7. [ Questions. ](#questions)  
+ 8. [ Deployment Location. ](#link)  
+ 9. [ Screenshot. ](#screenshot)  
+<a name='installation'></a> # Installation 
+ Installation Text  
+<a name='usage'></a> # Usage 
+ Usage Text  
+<a name='license'></a> # License 
+ License Text  
+<a name='contribution'></a> # Contribution Guidelines 
+ Guidelines Text  
+<a name='tests'></a> # Tests 
+ Test Text  
+<a name='tests'></a> # Tests 
+ Test Text  
+<a name='link'></a> # Deployment Location 
+ URL  
+<a name='screenshot'></a> # Screenshot: 
+ ![Screenshot](https://github.com/sean-mcelwain/weather_dashboard/blob/main/assets/images/screenshot.jpg)  
