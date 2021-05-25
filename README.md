f518f9aac43790e65aa907dba66865d4d8dce532
sean-mcelwain <79949547+sean-mcelwain@users.noreply.github.com>
2021-05-24 20:52:27 -0500

    updated index,js, 90% finished README Generator

--- README.md
+++ README.md
@@ -21,19 +21,20 @@ This utility uses command-line (node.js based) javascript, specifically node's f
 Once the dependencies are install, the README Generator app can be use through the command "node index.js." Once this comman is entered, then the inquirer package will prompt the user for information required to generate a README file.  The README file will be created in the dame local directory containing the file "index.js"
  <a name='license'></a>
  ## License 
-MIT
+The MIT License (MIT) 
+ Copyright © 2021 Sean McElwain. 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  <a name='contribution'></a>
  ## Contribution Guidelines 
-https://www.contributor-covenant.org/
+See https://www.contributor-covenant.org/
  <a name='tests'></a>
  ## Tests 
 No test are currently deployed for this project.
  <a name='questions'></a>
  ## Questions 
-For any question regarding this project, my preferred method of communication is via email at sean.mcelwain.KUBootcamp@gmail.com
- <a name='link'></a>
- ## Deployment Location 
-URL: https://https://github.com/sean-mcelwain/README_Generator
- <a name='screenshot'></a>
- ## Screenshot: 
-![Screenshot](Unavailable)  
+GitHub Profile: https://github.com/sean-mcelwain.  
+Contact Me: sean.mcelwain.KUBootcamp@gmail.com.  
+ 
+For any question regarding this project, my preferred method of communication is via email.
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Starts inquirer to prompt the user for input data
+const licenseArray = ["MIT License", "Apache License 2.0", "BSD License (3-Clause)"]
 
 inquirer.prompt([
   {
@@ -28,8 +29,19 @@ inquirer.prompt([
   },
   {
     name: "license_text",
+    type: "checkbox",
+    message: "Select your project's license:",
+    choices: licenseArray,
+  },
+  {
+    name: "year",
     type: "input",
-    message: "Enter your project's license information:",
+    message: "Enter your project's Copyright Year:",
+  },
+  {
+    name: "copyrightHolder",
+    type: "input",
+    message: "Enter the name of the Copyright Holder:",
   },
   {
     name: "contribution_text",
@@ -42,20 +54,21 @@ inquirer.prompt([
     message: "Enter your project's test information:",
   },
   {
+    name: "GitHub_link",
+    type: "input",
+    message: "Enter your GitHub username:",
+  },  
+  {
+    name: "email_text",
+    type: "input",
+    message: "Enter Your email address for questions: ",
+  },  
+  {
     name: "questions_text",
     type: "input",
     message: "Enter your project's question information:",
   },
-  {
-    name: "link_text",
-    type: "input",
-    message: "Enter your project's deployed location URL: https://",
-  },
-  {
-    name: "screenshot_text",
-    type: "input",
-    message: "Enter a URL to a screenshot of your project: ",
-  },
+
 ])
 .then((answer) => {
   console.log(answer.project_title);
@@ -66,12 +79,45 @@ let projectTitle = answer.project_title;
 let descriptionText = answer.description_text;
 let installationText = answer.installation_text;
 let usageText = answer.usage_text;
-let licenseText = answer.license_text;
+let licenseText;
+
+    let year = answer.year;
+    let copyrightHolder = answer.copyrightHolder;
+
+    const MIT_license = "The MIT License (MIT) \n Copyright © " + year +" "+ copyrightHolder + ". \n"
+    + "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n"
+    + "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n"
+    + "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+
+    const apacheLicense = "Copyright © " + year +" "+ copyrightHolder + ". \n"
+    + "Licensed under the Apache License, Version 2.0 (the 'License'); \n"
+    + "you may not use this file except in compliance with the License. \n"
+    + "You may obtain a copy of the License at: \n http://www.apache.org/licenses/LICENSE-2.0 \n"
+    + "Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. \n"
+    + "See the License for the specific language governing permissions and limitations under the License.";
+
+    const BSD_license = "Copyright © " + year +" "+ copyrightHolder + ".\n"
+    + "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: \n"
+    + " 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n"
+    + " 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n"
+    + " 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. \n"
+    + "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+
+    if (answer.license_text == "MIT License"){
+      licenseText = MIT_license;
+    }
+    if (answer.license_text == "Apache License 2.0"){
+      licenseText = apacheLicense;
+    }
+    if (answer.license_text == "BSD License (3-Clause)"){
+      licenseText = BSD_license;
+    };
+    
 let contributionText = answer.contribution_text;
 let testsText = answer.tests_text;
 let questionsText = answer.questions_text;
-let linkText = "URL: https://" + answer.link_text;
-let screenshotText = "![Screenshot](" + answer.screenshot_text + ")  \n";
+let linkText = "GitHub Profile: https://github.com/" + answer.GitHub_link + ".  \n";
+let emailText = "Contact Me: " + answer.email_text + ".  \n";
 
 // Creates variables for each section of the ReadMe and assigns them string values
 
@@ -94,14 +140,12 @@ const usage = "\n <a name='usage'></a>\n ## Usage \n" + usageText;
 const license = "\n <a name='license'></a>\n ## License \n" + licenseText;
 const contribution = "\n <a name='contribution'></a>\n ## Contribution Guidelines \n" + contributionText;
 const tests = "\n <a name='tests'></a>\n ## Tests \n" + testsText;
-const questions = "\n <a name='questions'></a>\n ## Questions \n" + questionsText;
-const link = "\n <a name='link'></a>\n ## Deployment Location \n" + linkText;
-const screenshot = "\n <a name='screenshot'></a>\n ## Screenshot: \n" + screenshotText;
+const questions = "\n <a name='questions'></a>\n ## Questions \n" + linkText + emailText + " \n" + questionsText;
 
 // Compiles a template ReadMe from the variables above. 
 
 const template = title + description + tableOfContents + installation + usage + license + contribution + tests
-                + questions + link + screenshot;
+                + questions;
 
 // Creates a function to write ReadMe file using the template
 function writeToFile(fileName, data) {
